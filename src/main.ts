371707bdfc2f302f953c1ea5364cf7caba58fe50
Cristian Paredes <csparedes@pucesi.edu.ec>
2024-01-10 14:07:30 -0500

    fix: ssl certs addeds

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,18 @@ import { AppModule } from './app.module'
 import { Logger, ValidationPipe } from '@nestjs/common'
 import helmet from 'helmet'
 import * as cookieParser from 'cookie-parser'
+import { readFileSync } from 'fs'
+import { join } from 'path'
 
 async function bootstrap() {
     const logger = new Logger('Main')
     const port = +process.env.PORT
-    const app = await NestFactory.create(AppModule)
+    const app = await NestFactory.create(AppModule, {
+        httpsOptions: {
+            cert: readFileSync(join(__dirname, '../ssl/pucei_edu_ec.crt')),
+            key: readFileSync(join(__dirname, '../ssl/pucei_edu_ec.key')),
+        },
+    })
     app.use(helmet())
     app.enableCors()
     app.use(cookieParser())
